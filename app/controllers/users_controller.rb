924fd0c8249590afdfe34a400781da52ab04be99
wangboguo <wangboguo@wangboguodeMBP.lan>
2016-12-15 15:17:48 +0800

    visitor registrate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to topics_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :topics, only: [:index, :show]
   resources :nodes, only: [:show]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :create]
+  get 'sign_up', to: 'users#new'
 end
