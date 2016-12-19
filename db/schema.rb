eae0fc6893caaf631873846a95f16db070f9c78b
wangboguo <wangboguo@wangboguodeMBP.lan>
2016-12-19 14:51:11 +0800

    user see topic last replied by info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219061811) do
+ActiveRecord::Schema.define(version: 20161219063421) do
 
   create_table "nodes", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161219061811) do
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "topics", force: :cascade do |t|
