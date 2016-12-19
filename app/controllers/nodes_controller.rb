7313c2a96be5ddc3fb2e347af77b5552ddade8d0
wangboguo <wangboguo@wangboguodeMBP.lan>
2016-12-19 14:10:45 +0800

    @node = Node.find_by(id: params[:id]) 可以写成 Node.find(params[:id]) 或者 @node = Node.find_by(id: params[:id])，不能@node = Node.find_by(params[:id]) 这样的话永远都是node(1)

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,6 +1,6 @@
 #
 class NodesController < ApplicationController
   def show
-    @node = Node.find_by(params[:id])
+    @node = Node.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
  #
  Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :topics, only: [:index, :show]
+  resources :topics, only: [:index, :show, :new, :create]
   resources :nodes, only: [:show]
   resources :users, only: [:show, :create]
   get 'sign_up', to: 'users#new'
