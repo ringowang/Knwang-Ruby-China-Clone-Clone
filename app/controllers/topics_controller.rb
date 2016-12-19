7154f338f9e4fdaf49a8ab668b8939eca67a8809
wangboguo <wangboguo@wangboguodeMBP.lan>
2016-12-19 11:41:52 +0800

    topic完善

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,23 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user_id = 1
+    if @topic.save
+      redirect_to topics_path
+    else 
+      render action: :new
+    end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title, :content, :node_id, :user_id)
+  end
 end
